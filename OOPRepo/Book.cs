753d6d93f6456f2d1dc0f638380bb7ff2606c6a9
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-10-28 16:13:24 +0500

    Update in Book.cs and Program.cs

--- OOPRepo/Book.cs
+++ OOPRepo/Book.cs
@@ -8,10 +8,87 @@ namespace OOPRepo
 {
     public class Book
     {
-        public void Hello()
+        //feild => maydon
+        private string name = "No name";
+        private string author = "No author";
+        private decimal price = 0;
+
+        //property => xususiyat
+        public string Name 
         {
-            Console.WriteLine("Uzbekona salom");
+            get
+            {
+                return name;
+            }
+            set 
+            {
+                name = value;
+            }
         }
+        public string Author 
+        {
+            get
+            {
+                return author;
+            }
+            set 
+            {
+                author = value;
+            } 
+        }
+
+        public decimal Price {
+            get
+            {
+                return price;
+            }
+            set 
+            {
+                price = value;
+            }
+        }
+        public string Edition { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+
+        public Book()
+        {
+            
+        }
+
+        public Book(string Name)
+        {
+            this.Name = Name;
+        }
+
+        //bu usul orqali biz Name prop qiymatini olamiz
+        public void GetName()
+        {
+            Console.WriteLine($"Name: {this.Name}");
+        }
+
+        //method new for prop
+        public void SetNewProperty()
+        {
+            Console.Write("Edition: ");
+            Edition = Console.ReadLine();
+            Console.Write("Description: ");
+            Description = Console.ReadLine();
+            Console.Write("Type: ");
+            Type = Console.ReadLine();
+        }
+        //Aded Method GetAllIfo()
+        public void GetAllInfo()
+        {
+            Console.WriteLine($"Name: {Name}\n" +
+                $"Author: {Author}\n" +
+                $"Price: {Price}\n" +
+                $"Edition: {Edition}\n" +
+                $"Description: {Description}\n" +
+                $"Type: {Type}");
+        }
+
+
     }
 
 }
