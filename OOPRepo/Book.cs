39f424ac18da51a21c4084604057a8bab5f8f64b
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-10-28 15:22:57 +0500

    Add Project in OOPRepo

---
+++ OOPRepo/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPRepo
+{
+    public class Book
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Uzbekona salom");
+        }
+    }
+
+}
