570e63109cc69c151f7773f2eb9fd9c2014daaa5
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-10-31 15:47:16 +0500

    Add answhere

---
+++ OOPRepo/AvtoSalon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPRepo
+{
+    enum AvtoSalon
+    {
+        Nexia = 90000000,
+        Damas = 80000000,
+        Gentra = 150000000
+
+    }
+}
---
+++ OOPRepo/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPRepo
+{
+    public static class Person
+    {
+        //Person person = new Person();
+        static int son = 12;
+        public static string FirstName { get; set; }
+        public static string LastName { get; set; }
+        public static int Age { get; set; }
+
+        public static void GetInfo()
+        {
+            Console.WriteLine("Ma'lumot berildi");
+        }
+
+
+    }
+}
